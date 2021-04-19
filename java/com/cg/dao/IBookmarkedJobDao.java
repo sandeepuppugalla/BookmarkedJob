944d7ec5c475e3c85bdf2e21fbc60fa69df3bee5
sandeepuppugalla <46489526+sandeepuppugalla@users.noreply.github.com>
2021-04-19 13:10:04 +0530

    Create IBookmarkedJobDao.java

---
+++ java/com/cg/dao/IBookmarkedJobDao.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+import com.cg.freelanceapp.entities.BookmarkedJob;
+import com.cg.freelanceapp.entities.Freelancer;
+import com.cg.freelanceapp.entities.Skill;
+
+public interface IBookmarkedJobDao {
+
+	BookmarkedJob save(BookmarkedJob job);
+
+	void remove(BookmarkedJob job);
+
+	BookmarkedJob findById(Long id);
+
+	List<BookmarkedJob> findBookmarkedJobsBySkill(Skill skill, Freelancer freelancer);
+	
+}
