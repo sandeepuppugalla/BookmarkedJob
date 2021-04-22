19573cba952fbe16fc57cb25b1d47a136fe04a2d
sandeepuppugalla <46489526+sandeepuppugalla@users.noreply.github.com>
2021-04-22 16:52:04 +0530

    Update BookmarkedJobDto.java

--- java/com/cg/DTO/BookmarkedJobDto.java
+++ java/com/cg/DTO/BookmarkedJobDto.java
@@ -1,1 +1,38 @@
+package com.cg.freelanceapp.dto;
 
+public class BookmarkedJobDTO 
+{
+	Long skillId;
+	Long freelancerId;
+	Long jobId;
+	public BookmarkedJobDTO() {
+		super();
+	}
+	public BookmarkedJobDTO(Long skillId, Long freelancerId, Long jobId) {
+		super();
+		this.skillId = skillId;
+		this.freelancerId = freelancerId;
+		this.jobId = jobId;
+	}
+	public Long getSkillId() {
+		return skillId;
+	}
+	public void setSkillId(Long skillId) {
+		this.skillId = skillId;
+	}
+	public Long getFreelancerId() {
+		return freelancerId;
+	}
+	public void setFreelancerId(Long freelancerId) {
+		this.freelancerId = freelancerId;
+	}
+	public Long getJobId() {
+		return jobId;
+	}
+	public void setJobId(Long jobId) {
+		this.jobId = jobId;
+	}
+	
+	
+
+}
